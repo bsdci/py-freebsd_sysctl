c4f28449075c33f0ed4dc09248bf54df76a1a7d4
Stefan Grönke <stefan@gronke.net>
2020-04-26 18:10:56 +0200

    read setup.py version from VERSION file or use git sha

--- setup.py
+++ setup.py
@@ -4,20 +4,32 @@ import os
 import os.path
 from setuptools import find_packages, setup
 
-cwd = os.getcwd()
 
-about = {}
-version_file = os.path.join(os.getcwd(), "freebsd_sysctl", '__version__.py')
-with open(version_file, encoding="utf-8") as f:
-    VERSION = exec(f.read(), about)  # nosec
+def read_first_line(relative_filename: str) -> str:
+    absolute_path = os.path.join(os.getcwd(), relative_filename)
+    with open(absolute_path, "r", encoding="UTF-8") as f:
+        return f.readline().strip()
 
-with open(os.path.join(cwd, 'README.md'), encoding='utf-8') as f:
+
+try:
+    VERSION = read_first_line("VERSION")
+except:
+    # last git commit SHA is the version
+    line = read_first_line(".git/HEAD")
+    if line.startswith("ref: ") is False:
+        VERSION = line
+    else:
+        ref = line[5:]
+        VERSION = read_first_line(f".git/{ref}")
+
+
+with open(os.path.join(cwd, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 
 setup(
     name="freebsd-sysctl",
-    version=about['__version__'],
+    version=VERSION,
     description="Native Python wrapper for FreeBSD sysctls using libc.",
     long_description=long_description,
     long_description_content_type="text/markdown",
