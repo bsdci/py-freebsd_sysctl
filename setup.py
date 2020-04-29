a4887d3d1569a522fa34584f61f5ec41a1a58a6a
Stefan Grönke <stefan@gronke.net>
2020-04-29 18:58:46 +0200

    depend on changelogmd for version info gathering

--- setup.py
+++ setup.py
@@ -4,6 +4,23 @@ import os
 import os.path
 from setuptools import find_packages, setup
 from freebsd_sysctl.__version__ import VERSION
+from changelogmd import Changelog
+
+
+def _read_requirements(
+    filename: str="requirements.txt"
+) -> typing.Dict[str, typing.List[str]]:
+    reqs = list(parse_requirements(filename, session="ioc_cli"))
+    return dict(
+        install_requires=list(map(lambda x: f"{x.name}{x.specifier}", reqs)),
+        dependency_links=list(map(
+            lambda x: str(x.link),
+            filter(lambda x: x.link, reqs)
+        ))
+    )
+
+
+_requirements = _read_requirements("requirements.txt")
 
 with open(
     os.path.join(os.path.dirname(__file__), "README.md"),
@@ -21,6 +38,8 @@ setup(
     author="Stefan Grönke",
     author_email="stefan@gronke.net",
     python_requires=">=3.6",
+    install_requires=_requirements["install_requires"],
+    dependency_links=_requirements["dependency_links"],
     tests_require=["pytest-runner", "pytest"],
     packages=find_packages(exclude=('tests',))
 )
